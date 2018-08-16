7d6d30cb2c4f5dcb95ac5618adbf4ce68ac4cf03
1395917451 <19960708zx>
2018-08-16 15:51:57 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-class Person {
-    constructor(name) {
+class People {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
     }
-    getName() {
-        return this.name;
+    eat() {
+        console.log(`${this.name} eat something`);
+    }
+    speak() {
+        console.log(`age ${this.name}`);
     }
 }
 
-let p = new Person("tony");
-alert(p.getName());
+class Student extends People {
+    constructor (name, age, number) {
+        super(name, age);
+        this.number = number;
+    }
+    study() {
+        console.log(`${this.name} study`)
+    }
+}
+
+let tony = new Student("tony", 20, "A1");
+let tom = new Student("tom", 18, "A2");
+
+tony.study();
+console.log(tony)
